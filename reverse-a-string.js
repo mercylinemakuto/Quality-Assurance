731b49df3a424309542a4ab19d27f29dfd5aea7d
Shabach Muriithi <shabachmuriithi@gmail.com>
2025-03-27 16:54:27 +0300

    reversed a string

---
+++ reverse-a-string.js
@@ -0,0 +1,5 @@
+// stringUtils.js
+function reverseString(str) {
+  return str.split('').reverse().join('');
+}
+module.exports = reverseString;
---
+++ reverse-a-string.test.js
@@ -0,0 +1,6 @@
+const reverseString = require("./reverse-a-string");
+
+test("reverse a string", () => {
+    expect(reverseString("Quality assuarance")).toBe("ecnaraussa ytilauQ");
+});
+
